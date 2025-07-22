54a2c476013239eea1ba95fca94fa50c17997b8e
K10-K10 <keito.tottori@gmail.com>
2025-07-22 15:30:04 +0900

    Fix switch's bug

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 	std::stack<int> sk;
 	char code[25][80];
 	int ptr_x = 0, ptr_y = 0;
-	bool flag_done = false;
+	bool flag_done = false, skip = false;
 	enum Direction
 	{
 		UP,
@@ -25,52 +25,69 @@ int main()
 	}
 	while (true)
 	{
-		if (a == UP)
+		if (!skip)
 		{
-			if (ptr_x == 0)
-				ptr_x = 24;
-			else
-				ptr_x--;
-		}
-		else if (a == DOWN)
-		{
-			if (ptr_x == 24)
-				ptr_x = 0;
-			else
-				ptr_x++;
-		}
-		else if (a == RIGHT)
-		{
-			if (ptr_y == 79)
-				ptr_y = 0;
-			else
-				ptr_y++;
-		}
-		else if (DOWN)
 			char c = code[ptr_x][ptr_y];
-		switch (c)
-		{
-		case '>':
-			a = RIGHT;
-			break;
-		case '<':
-			a = LEFT;
-			if (ptr_y == 0)
-				ptr_y = 79;
-			else
-				ptr_y--;
-			break;
-		case 'v':
-			a = DOWN;
-			break;
-		case '^':
-			a = UP;
-			break;
-		case '_':
-			if (sk.top() == 0)
+			switch (c)
 			{
-				sk.pop();
+			case '>':
+				a = RIGHT;
+				break;
+			case '<':
+				a = LEFT;
+				break;
+			case 'v':
+				a = DOWN;
+				break;
+			case '^':
+				a = UP;
+				break;
+
+			case '_':
+				if (!sk.empty())
+				{
+					if (sk.top() == 0)
+						a = RIGHT;
+					else
+						a = LEFT;
+					sk.pop();
+				}
+				break;
+
+			case '|':
+				if (!sk.empty())
+				{
+					if (sk.top() == 0)
+						a = DOWN;
+					else
+						a = UP;
+					sk.pop();
+				}
+				break;
+
+			case '#':
+				skip = true;
+				break;
+
+			case '@':
+				return 0;
+
+			default:
+				break;
 			}
 		}
+
+		// 移動処理
+		if (a == UP)
+			ptr_x = (ptr_x + 24) % 25;
+		else if (a == DOWN)
+			ptr_x = (ptr_x + 1) % 25;
+		else if (a == RIGHT)
+			ptr_y = (ptr_y + 1) % 80;
+		else if (a == LEFT)
+			ptr_y = (ptr_y + 79) % 80;
+
+		if (skip)
+			skip = false;
 	}
 }
