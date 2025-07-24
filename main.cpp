ed1092a4c26cf847483ef897791571b15284064e
K10-K10 <keito.tottori@gmail.com>
2025-07-24 09:28:18 +0900

    create

--- main.cpp
+++ main.cpp
@@ -7,7 +7,10 @@ int main()
 	std::stack<int> sk;
 	char code[25][80];
 	int ptr_x = 0, ptr_y = 0;
-	bool flag_done = false, skip = false;
+	int x = 0, y = 0, z = 0;
+	char input_char, code_char;
+	int input_int;
+	bool flag_done = false, skip = false, quote = false;
 	enum Direction
 	{
 		UP,
@@ -23,145 +26,196 @@ int main()
 			std::cin >> code[i][j];
 		}
 	}
+	for (int i = 0; i < 25; i++)
+	{
+		for (int j = 0; j < 80; j++)
+		{
+			std::cout << code[i][j];
+		}
+		std::cout << std::endl;
+	}
 	while (true)
 	{
 		if (!skip)
 		{
-			char c = code[ptr_x][ptr_y];
-			switch (c)
+			if (!quote)
 			{
-			case '>':
-				a = RIGHT;
-				break;
-			case '<':
-				a = LEFT;
-				break;
-			case 'v':
-				a = DOWN;
-				break;
-			case '^':
-				a = UP;
-				break;
-			case '_':
-				if (!sk.empty())
+				char c = code[ptr_x][ptr_y];
+				switch (c)
 				{
-					if (sk.top() == 0)
-						a = RIGHT;
-					else
-						a = LEFT;
+				case '>':
+					a = RIGHT;
+					break;
+				case '<':
+					a = LEFT;
+					break;
+				case 'v':
+					a = DOWN;
+					break;
+				case '^':
+					a = UP;
+					break;
+				case '_':
+					if (!sk.empty())
+					{
+						if (sk.top() == 0)
+							a = RIGHT;
+						else
+							a = LEFT;
+						sk.pop();
+					}
+					break;
+				case '|':
+					if (!sk.empty())
+					{
+						if (sk.top() == 0)
+							a = DOWN;
+						else
+							a = UP;
+						sk.pop();
+					}
+					break;
+				case '#':
+					skip = true;
+					break;
+				case '@':
+					return 0;
+				case '&':
+					std::cin >> input_int;
+					sk.push(input_int);
+					break;
+				case '~':
+					sk.push(int(input_char));
+					break;
+				case '.':
+					std::cout << sk.top() << " ";
 					sk.pop();
+					break;
+				case ',':
+					std::cout << char(sk.top());
+					sk.pop();
+					break;
+				case '+':
+					x = sk.top();
+					sk.pop();
+					y = sk.top();
+					sk.pop();
+					sk.push(x + y);
+					break;
+				case '-':
+					x = sk.top();
+					sk.pop();
+					y = sk.top();
+					sk.pop();
+					sk.push(y - x);
+					break;
+				case '/':
+					x = sk.top();
+					sk.pop();
+					y = sk.top();
+					sk.pop();
+					sk.push(y / x);
+					break;
+				case '*':
+					x = sk.top();
+					sk.pop();
+					y = sk.top();
+					sk.pop();
+					sk.push(y * x);
+					break;
+				case '%':
+					x = sk.top();
+					sk.pop();
+					y = sk.top();
+					sk.pop();
+					sk.push(y % x);
+					break;
+				case '`':
+					x = sk.top();
+					sk.pop();
+					y = sk.top();
+					sk.pop();
+					sk.push(y > x ? 1 : 0);
+					break;
+				case '!':
+					x = sk.top();
+					sk.pop();
+					sk.push(x == 0 ? 1 : 0);
+					break;
+				case ':':
+					x = sk.top();
+					sk.push(x);
+					break;
+				case '\\':
+					x = sk.top();
+					sk.pop();
+					y = sk.top();
+					sk.pop();
+					sk.push(x);
+					sk.push(y);
+					break;
+				case '$':
+					sk.pop();
+					break;
+				case 'g':
+					x = sk.top();
+					sk.pop();
+					y = sk.top();
+					sk.pop();
+					code_char = code[x][y];
+					sk.push(int(code_char));
+					break;
+				case 'p':
+					x = sk.top();
+					sk.pop();
+					y = sk.top();
+					sk.pop();
+					z = sk.top();
+					sk.pop();
+					code[x][y] = char(z);
+					break;
+				case '1':
+					sk.push(1);
+					break;
+				case '2':
+					sk.push(2);
+					break;
+				case '3':
+					sk.push(3);
+					break;
+				case '4':
+					sk.push(4);
+					break;
+				case '5':
+					sk.push(5);
+					break;
+				case '6':
+					sk.push(6);
+					break;
+				case '7':
+					sk.push(7);
+					break;
+				case '8':
+					sk.push(8);
+					break;
+				case '9':
+					sk.push(9);
+					break;
+				default:
+					break;
 				}
-				break;
-			case '|':
-				if (!sk.empty())
+			}
+			if (quote)
+			{
+				char c = code[ptr_x][ptr_y];
+				if (c == '"')
 				{
-					if (sk.top() == 0)
-						a = DOWN;
-					else
-						a = UP;
-					sk.pop();
+					quote = false;
 				}
-				break;
-			case '#':
-				skip = true;
-				break;
-			case '@':
-				return 0;
-			case '&':
-				int input;
-				std::cin >> input;
-				sk.push(input);
-				break;
-			case '~':
-				char input;
-				sk.push(int(input));
-				break;
-			case '.':
-				std::cout << sk.top() << " ";
-				sk.pop();
-				break;
-			case ',':
-				std::cout << char(sk.top());
-				sk.pop();
-				break;
-			case '+':
-				int x = sk.top();
-				sk.pop();
-				int y = sk.top();
-				sk.pop();
-				sk.push(x + y);
-				break;
-			case '-':
-				int x = sk.top();
-				sk.pop();
-				int y = sk.top();
-				sk.pop();
-				sk.push(y - x);
-				break;
-			case '/':
-				int x = sk.top();
-				sk.pop();
-				int y = sk.top();
-				sk.pop();
-				sk.push(y / x);
-				break;
-			case '*':
-				int x = sk.top();
-				sk.pop();
-				int y = sk.top();
-				sk.pop();
-				sk.push(y * x);
-				break;
-			case '%':
-				int x = sk.top();
-				sk.pop();
-				int y = sk.top();
-				sk.pop();
-				sk.push(y % x);
-				break;
-			case '`':
-				int x = sk.top();
-				sk.pop();
-				int y = sk.top();
-				sk.pop();
-				sk.push(y > x ? 1 : 0);
-				break;
-			case '!':
-				int x = sk.top();
-				sk.pop();
-				sk.push(x == 0 ? 1 : 0);
-				break;
-			case ':':
-				int x = sk.top();
-				sk.push(x);
-				break;
-			case '\\':
-				int x = sk.top();
-				sk.pop();
-				int y = sk.top();
-				sk.pop();
-				sk.push(x);
-				sk.push(y);
-				break;
-			case '$':
-				sk.pop();
-			case 'g':
-				int x = sk.top();
-				sk.pop();
-				int y = sk.top();
-				sk.pop();
-				char code_char = code[x][y];
-				sk.push(int(code_char));
-			case 'p':
-				int x = sk.top();
-				sk.pop();
-				int y = sk.top();
-				sk.pop();
-				int z = sk.top();
-				sk.pop();
-				code[x][y] = char(z);
-			default:
-				break;
+				else
+				{
+					sk.push(int(c));
+				}
 			}
 		}
 
@@ -173,8 +227,9 @@ int main()
 			ptr_y = (ptr_y + 1) % 80;
 		else if (a == LEFT)
 			ptr_y = (ptr_y + 79) % 80;
-
+		std::cout << ptr_x << " " << ptr_y << std::endl;
 		if (skip)
 			skip = false;
 	}
+	return 0;
 }
