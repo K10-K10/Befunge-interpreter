f78964dd4aa8e73c7416934f73e76241cb401124
rotarymars <s.goto2050@gmail.com>
2025-07-24 22:10:26 +0900

    added test

---
+++ .github/workflows/scripts/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd .github/workflows/scripts/tests
+
+bin=../../../../bef
+
+for file in *.in; do
+    filename=${file%.in}
+    echo "Testing $filename"
+    $bin $file > $filename.out.swap
+    flag=0
+    if diff $filename.out.swap $filename.out; then
+        echo "Test $filename passed"
+    else
+        echo "Test $filename failed"
+        flag=1
+    fi
+    rm $filename.out.swap
+    if [ $flag -eq 1 ]; then
+        exit 1
+    fi
+done
